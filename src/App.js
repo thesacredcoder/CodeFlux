c2b30ea6377d745fb4fa02091282d39b8903a30f
thesacredcoder <akashsamal4875@gmail.com>
2021-12-20 21:41:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Logo from "./assets/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex ">
+      <div className="bg-main hover:bg-second h-screen w-1/2 flex justify-center items-center">
+        <div>
+          <h1 className="text-white font-brand text-7xl font-medium mb-2">
+            CODE <br />
+            FLUX
+          </h1>
+          <p className="text-white font-accent text-lg">
+            We got something for Everybody
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center w-1/2">
+        <form className="flex flex-col w-full justify-center items-center">
+          <input
+            type="text"
+            placeholder="Name"
+            className="mb-4 p-4 w-1/2 border-2 border-second rounded-lg outline-main"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            className="mb-4 p-4 w-1/2 border-2 border-second rounded-lg outline-main"
+          />
+          <button className="bg-second p-4 rounded-lg text-white font-brand uppercase font-bold tracking-wider w-1/2 hover:bg-main ease-out transition-all">
+            Register Now
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#3c55ff",
+        second: "#fa643f",
+      },
+      fontFamily: {
+        brand: "'Barlow', sans-serif",
+        accent: "'Open Sans', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
