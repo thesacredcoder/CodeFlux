0b0823261674dcf2aaa25b372c3a973b898b0426
Mohak Sharma <mohak327@gmail.com>
2021-12-21 01:44:15 +0530

    added logo on mobile

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ import leftbg1 from './assets/leftbg1.svg'
 function App() {
   return (
     <div className='flex justify-center items-center'>
-      <div className='left w-0 hover:bg-second h-screen sm:w-1/2  lg:w-2/5 flex flex-col justify-center items-center'>
+      <div className='left w-0 hover:bg-second h-screen md:w-1/2 lg:w-2/5 flex flex-col justify-center items-center'>
         <img src={Logo} alt='' />
         <p className='text-white font-accent text-lg'>
           We got something for everybody
         </p>
       </div>
-      <div className='flex flex-col justify-center items-center w-4/5 sm:w-1/2 lg:w-3/5'>
+      <div className='flex flex-col justify-center items-center w-4/5 sm:w-3/5 md:w-1/2 lg:w-3/5'>
         <div className='flex flex-col justify-center items-center mb-16'>
-          {/* <h1 className='title text-black font-brand text-7xl font-medium mb-2'>
-            CODE <br />
-            FLUX
-          </h1> */}
-          <div className='mb-0'>
+          {/* <h5 className='fixed top-5 right-5 title text-black font-brand text-3xl font-medium mb-2 uppercase'>
+            codeflux
+          </h5> */}
+          <img className='fixed top-5 right-5 h-16 md:hidden' src={Logo} alt='' />
+
+          <div className='mb-0 h-[250px] w-[300px] lg:h-[250px] lg:w-[300px] md:h-[200px] md:w-[250px]'>
             {/* <img src={Logo} alt="" /> */}
-            <img src={leftbg1} height='300px' width='300px' alt='' />
+            <img src={leftbg1} alt='' />
           </div>
 
         </div>
